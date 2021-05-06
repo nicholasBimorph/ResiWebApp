f1296707bed706f5225528e8b89b39419fdbfff5
nicholasBimorph <nicholas@bimorph.com>
2021-05-06 19:27:24 +0100

    commit before checkout

--- HBResi/wwwroot/js/scene.js
+++ HBResi/wwwroot/js/scene.js
@@ -1,10 +1,15 @@
-﻿var scene;
+﻿
+
+
+var scene;
 var camera;
 var cube;
 var renderer;
 var material;
 var caller;
 var container;
+var controls;
+
 
 function createScene(reference) {
 
@@ -12,33 +17,66 @@ function createScene(reference) {
     if (!container) {
         return;
     }
-   
+
     caller = reference;
     scene = new THREE.Scene();
+    scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+    scene.background = new THREE.Color(0xa0a0a0);
     camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-    renderer = new THREE.WebGLRenderer();
+    camera.position.z = 5;
+  
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(container.clientWidth, container.clientHeight);
-   // document.body.appendChild(renderer.domElement);
 
-   while (container.lastElementChild) {
-       container.removeChild(container.lastElementChild);
+
+
+    var gridHelper = new THREE.GridHelper(10, 20);
+    scene.add(gridHelper);
+
+    const dirLight1 = new THREE.DirectionalLight(0xffddcc, 1);
+    dirLight1.position.set(1, 0.75, 0.5);
+    scene.add(dirLight1);
+
+    const dirLight2 = new THREE.DirectionalLight(0xccccff, 1);
+    dirLight2.position.set(- 1, 0.75, - 0.5);
+    scene.add(dirLight2);
+
+    // container
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
     }
 
-   container.appendChild(renderer.domElement);
-    
+  
+    container.appendChild(renderer.domElement);
+
 }
 
 function createCube() {
-    var geometry = new THREE.BoxGeometry();
-    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    cube = new THREE.Mesh(geometry, material);
-    cube.callback = function () { caller.invokeMethodAsync('OnClickCube', cube); };
+
+    const geometry = new THREE.TorusKnotGeometry();
+
+     material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true });
+
+     cube = new THREE.Mesh(geometry, material);
+
     scene.add(cube);
-    camera.position.z = 5;
+
+    //controls = new OrbitControls(camera, renderer.domElement);
+    //controls.screenSpacePanning = true;
+    //controls.minDistance = 5;
+    //controls.maxDistance = 40;
+    //controls.target.set(0, 2, 0);
+    //controls.update();
 }
 
 var animate = function () {
     requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.007;
+    cube.rotation.y += 0.007;
+
     renderer.render(scene, camera);
+
 };
